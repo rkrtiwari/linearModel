b825929665addba26cf049dec440977bec9c5bdc
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2016-05-17 07:27:26 +0800

    added linear model

--- server.R
+++ server.R
@@ -5,7 +5,12 @@ server <- function(input, output){
   output$plot1 <- renderPlot({
     x <- mtcars[[input$"var1"]]
     y <- mtcars[[input$"var2"]]
+    
+    mtModel <- lm(y ~ x)
+    ypred <- mtModel$coefficients[1] + x*mtModel$coefficients[2]
+    
     plot(x, y, cex = 1.5, col = "red", pch = 19)
+    lines(x, ypred, col = "blue")
   })
   
  output$corVal <- renderPrint({
