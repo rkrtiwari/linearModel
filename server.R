b78643976453f07ff3929808dea2014707ec75cf
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2016-05-17 09:52:04 +0800

    Using reactive function to avoid multiple calculation of the same expression

--- server.R
+++ server.R
@@ -2,21 +2,20 @@ library(shiny)
 
 server <- function(input, output){
   
+  x <- reactive({as.numeric(mtcars[[input$"var1"]])})
+  y <- reactive({as.numeric(mtcars[[input$"var1"]])})
+  
   output$plot1 <- renderPlot({
-    x <- mtcars[[input$"var1"]]
-    y <- mtcars[[input$"var2"]]
     
-    mtModel <- lm(y ~ x)
-    ypred <- mtModel$coefficients[1] + x*mtModel$coefficients[2]
+    mtModel <- lm(y() ~ x())
+    ypred <- mtModel$coefficients[1] + x()*mtModel$coefficients[2]
     
-    plot(x, y, cex = 1.5, col = "red", pch = 19)
-    lines(x, ypred, col = "blue")
+    plot(x(), y(), cex = 1.5, col = "red", pch = 19)
+    lines(x(), ypred, col = "blue")
   })
   
  output$corVal <- renderPrint({
-   x <- mtcars[[input$"var1"]]
-   y <- mtcars[[input$"var2"]]
-   cor(x,y)
+   cor(x(),y())
  }
  )
 }
